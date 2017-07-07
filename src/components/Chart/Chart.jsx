8e3bc6c2e7ae17fcbdd27e6e39ce75f11b1f17a8
Kari Sabine Malmin <karisabine@gmail.com>
2017-07-07 15:19:23 +0200

    Added figure/table display option

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -20,8 +20,14 @@ export default class Charts extends React.Component {
     super(props);
     this.state = {
       selectedType: this.props.type,
-      items: this.props.data
+      items: this.props.data,
+      selectedVisning: 'figur'
     }
+  }
+
+  handleVisning(e) {
+    console.log(e.target.value);
+    this.setState({selectedVisning: e.target.value})
   }
 
   handleSelect(changeEvent) {
@@ -54,24 +60,19 @@ export default class Charts extends React.Component {
     }
 
     const newDrilldown = []
-    const drilldown = update(newDrilldown, {$push:
-      this.state.items.map((item) => (
-        {
-          'id': item.id,
-          'data': item.data
-        }
-      ))
+    const drilldown = update(newDrilldown, {
+      $push: this.state.items.map((item) => ({'id': item.id, 'data': item.data}))
     })
 
     const newSeries = []
-    const series = update(newSeries, {$push:
-      this.state.items.map((item) => (
-        {
-          'name': item.name,
-          'y': item.y,
-          'drilldown': item.data ? item.id : null
-        }
-      ))
+    const series = update(newSeries, {
+      $push: this.state.items.map((item) => ({
+        'name': item.name,
+        'y': item.y,
+        'drilldown': item.data
+          ? item.id
+          : null
+      }))
     })
 
     new Highcharts.chart(this.props.title, {
@@ -111,48 +112,65 @@ export default class Charts extends React.Component {
 
   render() {
 
-    const tabell = this.state.items.map((item) =>
-      <tbody>
-        <tr>
-          <th rowSpan={item.data ? item.data.length + 1 : null}>
-            {item.name}
-          </th>
-          <td rowSpan={item.data ? item.data.length + 1 : null}>
-            {item.y}
-          </td>
-        </tr>
-        {item.data ? item.data.map((item) =>
-          <tr>
-            <th>{item.name}</th>
-            <td>{item.y}</td>
-          </tr>
-        ) : null}
-      </tbody>
-    )
+    const tabell = this.state.items.map((item) => <tbody>
+      <tr>
+        <th rowSpan={item.data
+          ? item.data.length + 1
+          : null}>
+          {item.name}
+        </th>
+        <td rowSpan={item.data
+          ? item.data.length + 1
+          : null}>
+          {item.y}
+        </td>
+      </tr>
+      {item.data
+        ? item.data.map((item) => <tr>
+          <th>{item.name}</th>
+          <td>{item.y}</td>
+        </tr>)
+        : null}
+    </tbody>)
 
     return (
       <div className="highchart">
-        <div style={{
-          'width': '50%',
-          'display': 'inline-block',
-          'verticalAlign': 'bottom'
-        }}>
 
-          <DropdownList onChange={this.dropdownChange.bind(this)} defaultValue={this.state.selectedType} data={colors}/>
-          <Panel title={this.props.title} datagrunnlag={this.props.datagrunnlag} selectedPanel={this.props.selectedPanel} handleSlide={this.props.handleSlide.bind(this)}/>
-          <div id={this.props.title}></div>
+        <h2>{this.props.title}</h2>
 
+        <label><input type="radio" name={this.props.title + '_visning'} value="figur" checked="true" onChange={this.handleVisning.bind(this)} checked={this.state.selectedVisning === 'figur'}/>
+          Figur</label>
+        <label><input type="radio" name={this.props.title + '_visning'} value="tabell" onChange={this.handleVisning.bind(this)} checked={this.state.selectedVisning === 'tabell'}/>
+          Tabell</label>
+
+        <DropdownList onChange={this.dropdownChange.bind(this)} defaultValue={this.state.selectedType} data={colors}/>
+        <Panel title={this.props.title} datagrunnlag={this.props.datagrunnlag} selectedPanel={this.props.selectedPanel} handleSlide={this.props.handleSlide.bind(this)}/>
+
+        <div>
+
+          <div style={{
+            height: this.state.selectedVisning === 'figur'
+              ? 'auto'
+              : '0',
+            opacity: this.state.selectedVisning === 'figur'
+              ? '1'
+              : '0'
+          }}>
+            <div id={this.props.title}></div>
+          </div>
         </div>
 
         <div style={{
-          'width': '45%',
-          'display': 'inline-block',
-          'verticalAlign': 'bottom',
-          'margin': '0 2.5%'
+          height: this.state.selectedVisning === 'tabell'
+            ? 'auto'
+            : '0',
+          opacity: this.state.selectedVisning === 'tabell'
+            ? '1'
+            : '0'
         }}>
 
           <table>
-            <caption>Tabell-tittel</caption>
+            <caption>{this.props.title}</caption>
             {tabell}
           </table>
 
